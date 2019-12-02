30a9fcd3894fd87ed4c263a85d53900ee45456f9
Luke Brobbin <lukemichaelvocalist@gmail.com>
2019-12-02 20:27:56 +0000

    added part 2 solution

--- day-2-program-alarm/goLang/intCode.go
+++ day-2-program-alarm/goLang/intCode.go
@@ -1,16 +1,18 @@
 package main
 
+import "fmt"
+
 type intCode []int
 
-func (ic intCode) posOneAndTwo(i int) (int, int) {
+func (ic intCode) parameters(i int) (int, int) {
 	return ic[ic[i+1]], ic[ic[i+2]]
 }
 func (ic intCode) handleOne(i int) {
-	first, second := ic.posOneAndTwo(i)
+	first, second := ic.parameters(i)
 	ic[ic[i+3]] = first + second
 }
 func (ic intCode) handleTwo(i int) {
-	first, second := ic.posOneAndTwo(i)
+	first, second := ic.parameters(i)
 	ic[ic[i+3]] = first * second
 }
 
@@ -26,4 +28,27 @@ func (ic intCode) run() []int {
 		}
 	}
 	return ic
+}
+
+func partTwo(ic intCode, n int) {
+	for i := 0; i <= 100; i++ {
+		for j := 0; j <= 100; j++ {
+			p := append(ic[:0:0], ic...)
+			p[1] = i
+			p[2] = j
+			for k := 0; k < len(p); k += 4 {
+				switch p[k] {
+				case 1:
+					p.handleOne(k)
+				case 2:
+					p.handleTwo(k)
+				case 99:
+					break
+				}
+				if p[0] == n {
+					fmt.Println(p[1], p[2])
+				}
+			}
+		}
+	}
 }
--- day-2-program-alarm/goLang/main.go
+++ day-2-program-alarm/goLang/main.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-
+	partTwo(getCodes(), 19690720)
 }
