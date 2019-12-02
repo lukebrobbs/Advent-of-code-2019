2cda1c64ca6a0da21cf9bc3de877d39816c46ed2
Luke Brobbin <lukemichaelvocalist@gmail.com>
2019-12-02 18:03:42 +0000

    Begin work on day 2

--- day-2-program-alarm/goLang/intCode.go
+++ day-2-program-alarm/goLang/intCode.go
@@ -2,7 +2,7 @@ package main
 
 type intCode []int
 
-func (ic intCode) getIndexes(i int) (int, int) {
+func (ic intCode) posOneAndTwo(i int) (int, int) {
 	return ic[ic[i+1]], ic[ic[i+2]]
 }
 
@@ -10,10 +10,10 @@ func (ic intCode) run() []int {
 	for i := 0; i < len(ic); i += 4 {
 		switch ic[i] {
 		case 1:
-			first, second := ic.getIndexes(i)
+			first, second := ic.posOneAndTwo(i)
 			ic[ic[i+3]] = first + second
 		case 2:
-			first, second := ic.getIndexes(i)
+			first, second := ic.posOneAndTwo(i)
 			ic[ic[i+3]] = first * second
 		case 99:
 			break
