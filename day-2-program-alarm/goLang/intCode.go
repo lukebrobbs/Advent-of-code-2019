4f9802ab5d4057e508af5773ab2c19de5c87b32f
Luke Brobbin <lukemichaelvocalist@gmail.com>
2019-12-02 18:11:11 +0000

    refactor

--- day-2-program-alarm/goLang/intCode.go
+++ day-2-program-alarm/goLang/intCode.go
@@ -5,19 +5,24 @@ type intCode []int
 func (ic intCode) posOneAndTwo(i int) (int, int) {
 	return ic[ic[i+1]], ic[ic[i+2]]
 }
+func (ic intCode) handleOne(i int) {
+	first, second := ic.posOneAndTwo(i)
+	ic[ic[i+3]] = first + second
+}
+func (ic intCode) handleTwo(i int) {
+	first, second := ic.posOneAndTwo(i)
+	ic[ic[i+3]] = first * second
+}
 
 func (ic intCode) run() []int {
 	for i := 0; i < len(ic); i += 4 {
 		switch ic[i] {
 		case 1:
-			first, second := ic.posOneAndTwo(i)
-			ic[ic[i+3]] = first + second
+			ic.handleOne(i)
 		case 2:
-			first, second := ic.posOneAndTwo(i)
-			ic[ic[i+3]] = first * second
+			ic.handleTwo(i)
 		case 99:
 			break
-		default:
 		}
 	}
 	return ic
