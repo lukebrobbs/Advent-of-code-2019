7e1aa29c64d6118567aeb663caa56350d8387d09
Luke Brobbin <lukemichaelvocalist@gmail.com>
2019-12-03 17:41:39 +0000

    added solution to part one

--- day-3-crossed-wires/calculateDistance.go
+++ day-3-crossed-wires/calculateDistance.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"reflect"
 	"strconv"
 )
@@ -14,31 +15,20 @@ func handleDirection(d string, cp *[][]int) {
 	if err != nil {
 		fmt.Println("Unable to convert string to int")
 	}
-	switch string(d[0]) {
-	case "R":
-		for i := 0; i <= a; i++ {
-			last := c[len(c)-1]
+	for i := 0; i < a; i++ {
+		last := c[len(c)-1]
+		switch string(d[0]) {
+		case "R":
 			newItem = []int{last[0] + 1, last[1]}
-			c = append(c, newItem)
-		}
-	case "L":
-		for i := 0; i <= a; i++ {
-			last := c[len(c)-1]
+		case "L":
 			newItem = []int{last[0] - 1, last[1]}
-			c = append(c, newItem)
-		}
-	case "U":
-		for i := 0; i <= a; i++ {
-			last := c[len(c)-1]
+		case "U":
 			newItem = []int{last[0], last[1] + 1}
-			c = append(c, newItem)
-		}
-	case "D":
-		for i := 0; i <= a; i++ {
-			last := c[len(c)-1]
+		case "D":
 			newItem = []int{last[0], last[1] - 1}
-			c = append(c, newItem)
 		}
+		c = append(c, newItem)
+
 	}
 	*cp = append(c)
 }
@@ -51,20 +41,22 @@ func drawCoordinates(d []string) [][]int {
 	return c
 }
 
-func calculateDistance(wireOne []string, wireTwo []string) {
+func calculateDistance(wireOne []string, wireTwo []string) int {
 	coordsOne := drawCoordinates(wireOne)
 	coordsTwo := drawCoordinates(wireTwo)
-	// fmt.Println(coordsOne)
-	// fmt.Println(coordsTwo)
-
-	x := [][]int{}
+	shortest := 0
 	for _, c := range coordsTwo {
 		for _, d := range coordsOne {
 			if reflect.DeepEqual(c, d) {
-				fmt.Println("Tague", c)
-				x = append(x, c)
+				dist := int(math.Abs(float64(c[0])) + math.Abs(float64(c[1])))
+				if shortest == 0 {
+					shortest = dist
+				}
+				if dist < shortest {
+					shortest = dist
+				}
 			}
 		}
 	}
-	fmt.Println(x)
+	return shortest
 }
--- day-3-crossed-wires/calculateDistance_test.go
+++ day-3-crossed-wires/calculateDistance_test.go
@@ -1,77 +1,87 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
-// func TestIfDirectionIsRightShouldAddIncreasedXCoordinate(t *testing.T) {
-// 	d := []string{}
-// 	d = append(d, "R1")
-// 	expected := [][]int{[]int{0, 0}, []int{1, 0}}
-// 	actual := drawCoordinates(d)
-// 	if !reflect.DeepEqual(expected, actual) {
-// 		t.Fatalf("expected %v, instead got %v", expected, actual)
-// 	}
-// 	e := []string{}
-// 	e = append(e, "R5")
-// 	otherExpected := [][]int{[]int{0, 0}, []int{5, 0}}
-// 	otherActual := drawCoordinates(e)
-// 	if !reflect.DeepEqual(otherExpected, otherActual) {
-// 		t.Fatalf("expected %v, instead got %v", otherExpected, otherActual)
-// 	}
-// }
+func TestIfDirectionIsRightShouldAddIncreasedXCoordinate(t *testing.T) {
+	d := []string{}
+	d = append(d, "R1")
+	expected := [][]int{[]int{0, 0}, []int{1, 0}}
+	actual := drawCoordinates(d)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("expected %v, instead got %v", expected, actual)
+	}
+	e := []string{}
+	e = append(e, "R5")
+	otherExpected := [][]int{[]int{0, 0}, []int{1, 0}, []int{2, 0}, []int{3, 0}, []int{4, 0}, []int{5, 0}}
+	otherActual := drawCoordinates(e)
+	if !reflect.DeepEqual(otherExpected, otherActual) {
+		t.Fatalf("expected %v, instead got %v", otherExpected, otherActual)
+	}
+}
 
-// func TestIfDirectionIsLeftShouldAddDecreasedXCoordinate(t *testing.T) {
-// 	d := []string{}
-// 	d = append(d, "L1")
-// 	expected := [][]int{[]int{0, 0}, []int{-1, 0}}
-// 	actual := drawCoordinates(d)
-// 	if !reflect.DeepEqual(expected, actual) {
-// 		t.Fatalf("expected %v, instead got %v", expected, actual)
-// 	}
-// 	e := []string{}
-// 	e = append(e, "L5")
-// 	otherExpected := [][]int{[]int{0, 0}, []int{-5, 0}}
-// 	otherActual := drawCoordinates(e)
-// 	if !reflect.DeepEqual(otherExpected, otherActual) {
-// 		t.Fatalf("expected %v, instead got %v", otherExpected, otherActual)
-// 	}
-// }
+func TestIfDirectionIsLeftShouldAddDecreasedXCoordinate(t *testing.T) {
+	d := []string{}
+	d = append(d, "L1")
+	expected := [][]int{[]int{0, 0}, []int{-1, 0}}
+	actual := drawCoordinates(d)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("expected %v, instead got %v", expected, actual)
+	}
+	e := []string{}
+	e = append(e, "L5")
+	otherExpected := [][]int{[]int{0, 0}, []int{-1, 0}, []int{-2, 0}, []int{-3, 0}, []int{-4, 0}, []int{-5, 0}}
+	otherActual := drawCoordinates(e)
+	if !reflect.DeepEqual(otherExpected, otherActual) {
+		t.Fatalf("expected %v, instead got %v", otherExpected, otherActual)
+	}
+}
 
-// func TestIfDirectionIsUpShouldAddIncreasedYCoordinate(t *testing.T) {
-// 	d := []string{}
-// 	d = append(d, "U1")
-// 	expected := [][]int{[]int{0, 0}, []int{0, 1}}
-// 	actual := drawCoordinates(d)
-// 	if !reflect.DeepEqual(expected, actual) {
-// 		t.Fatalf("expected %v, instead got %v", expected, actual)
-// 	}
-// 	e := []string{}
-// 	e = append(e, "U5")
-// 	otherExpected := [][]int{[]int{0, 0}, []int{0, 5}}
-// 	otherActual := drawCoordinates(e)
-// 	if !reflect.DeepEqual(otherExpected, otherActual) {
-// 		t.Fatalf("expected %v, instead got %v", otherExpected, otherActual)
-// 	}
-// }
+func TestIfDirectionIsUpShouldAddIncreasedYCoordinate(t *testing.T) {
+	d := []string{}
+	d = append(d, "U1")
+	expected := [][]int{[]int{0, 0}, []int{0, 1}}
+	actual := drawCoordinates(d)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("expected %v, instead got %v", expected, actual)
+	}
+	e := []string{}
+	e = append(e, "U5")
+	otherExpected := [][]int{[]int{0, 0}, []int{0, 1}, []int{0, 2}, []int{0, 3}, []int{0, 4}, []int{0, 5}}
+	otherActual := drawCoordinates(e)
+	if !reflect.DeepEqual(otherExpected, otherActual) {
+		t.Fatalf("expected %v, instead got %v", otherExpected, otherActual)
+	}
+}
 
-// func TestIfDirectionIsDownShouldAddDecreasedYCoordinate(t *testing.T) {
-// 	d := []string{}
-// 	d = append(d, "D1")
-// 	expected := [][]int{[]int{0, 0}, []int{0, -1}}
-// 	actual := drawCoordinates(d)
-// 	if !reflect.DeepEqual(expected, actual) {
-// 		t.Fatalf("expected %v, instead got %v", expected, actual)
-// 	}
-// 	e := []string{}
-// 	e = append(e, "D5")
-// 	otherExpected := [][]int{[]int{0, 0}, []int{0, -5}}
-// 	otherActual := drawCoordinates(e)
-// 	if !reflect.DeepEqual(otherExpected, otherActual) {
-// 		t.Fatalf("expected %v, instead got %v", otherExpected, otherActual)
-// 	}
-// }
+func TestIfDirectionIsDownShouldAddDecreasedYCoordinate(t *testing.T) {
+	d := []string{}
+	d = append(d, "D1")
+	expected := [][]int{[]int{0, 0}, []int{0, -1}}
+	actual := drawCoordinates(d)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("expected %v, instead got %v", expected, actual)
+	}
+	e := []string{}
+	e = append(e, "D5")
+	otherExpected := [][]int{[]int{0, 0}, []int{0, -1}, []int{0, -2}, []int{0, -3}, []int{0, -4}, []int{0, -5}}
+	otherActual := drawCoordinates(e)
+	if !reflect.DeepEqual(otherExpected, otherActual) {
+		t.Fatalf("expected %v, instead got %v", otherExpected, otherActual)
+	}
+}
 
 func TestReturnsLowestCrossover(t *testing.T) {
-	calculateDistance([]string{"U7", "R6", "D4", "L4"}, []string{"R8", "U5", "L5", "D3"})
+	// expected := 159
+	// actual := calculateDistance([]string{"R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"}, []string{"U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"})
+	// if actual != expected {
+	// 	t.Fatalf("expected %v, instead got %v", expected, actual)
+	// }
+	// otherExpected := 135
+	// otherActual := calculateDistance([]string{"R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"}, []string{"U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"})
+	// if otherActual != otherExpected {
+	// 	t.Fatalf("expected %v, instead got %v", otherExpected, otherActual)
+	// }
 }
