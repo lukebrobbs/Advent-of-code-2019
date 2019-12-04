40f129f3c4bf3bcdedda9637c590f71e8adafeaa
Luke Brobbin <lukemichaelvocalist@gmail.com>
2019-12-04 10:47:48 +0000

    Updated failing tests

--- day-4-secure-container/secureContainer_test.go
+++ day-4-secure-container/secureContainer_test.go
@@ -4,18 +4,18 @@ import "testing"
 
 func TestShouldCountNumbersWith6Digits(t *testing.T) {
 	expected := secureContainer(1, 100000)
-	expectedTwo := secureContainer(111111, 111112)
+	expectedTwo := secureContainer(111111, 111122)
 	if expected != 0 {
 		t.Fatalf("Expected 1, instead recieved %v", expected)
 	}
-	if expectedTwo != 2 {
+	if expectedTwo != 1 {
 		t.Fatalf("Expected 3, instead recieved %v", expectedTwo)
 	}
 }
 
 func TestShouldOnlyCountIntsWithTwoAdjacentDigitsTheSame(t *testing.T) {
 	expected := secureContainer(111111, 111122)
-	if expected != 10 {
+	if expected != 1 {
 		t.Fatalf("Expected 3, instead recieved %v", expected)
 	}
 }
